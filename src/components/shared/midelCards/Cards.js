21583bfafc82bac8567b9d0e2a0d8067f3fcc6c8
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2023-12-27 12:21:38 +0330

    category card completed

--- src/components/shared/midelCards/Cards.js
+++ src/components/shared/midelCards/Cards.js
@@ -1,10 +1,11 @@
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+
+import styles from './Cards.module.css'
 
 import Image1 from "../../../assets/SliderPhoto/Gallery01.png";
 import Image2 from "../../../assets/SliderPhoto/3.jpg";
 import Image3 from "../../../assets/SliderPhoto/electronics.jpg";
 import Image4 from "../../../assets/SliderPhoto/jewelery.jpg";
+import { Link } from "react-router-dom";
 // import logo from "../assest/photos/logo.webp";
 
 const Cards = [
@@ -37,22 +38,21 @@ const Cards = [
 function CardsGallery() {
   return (
     
-        <Card style={{ display:"grid" , gridTemplateColumns:"repeat(4,1fr)" ,
-      }} >
+        <div  className={styles.container} >
       {Cards.map((card) => (
-        <Card style={{border:"1px solid silver" , borderRadius:"5px" , margin:"20px", padding:"15px",justifyContent:"space-between" }}>
-          <Card.Img variant="top" src={card.image} style={{ width: "15rem" }} />
-          <Card.Body>
-            <Card.Title>{card.title}</Card.Title>
-            <Card.Text>
+        <div className={styles.main}>
+          <img  className={styles.productImage} src={card.image}  />
+          <div>
+            <h3>{card.title}</h3>
+            <p>
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-             </Card>
+            </p>
+            <Link to="/">{card.title}</Link>
+          </div>
+             </div>
       ))}
-      </Card>
+      </div>
    
   );
 }
